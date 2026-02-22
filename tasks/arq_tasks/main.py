bffd000abb197b4048a963bd8283aee4b9f44f47
Siva-Rajan-R <siva967763@gmail.com>
2026-02-23 00:09:21 +0530

    updated the arq tasks

--- tasks/arq_tasks/main.py
+++ tasks/arq_tasks/main.py
@@ -1,15 +1,21 @@
 # worker.py
-from arq import create_pool
+import asyncio
+from arq import Worker
 from arq.connections import RedisSettings
-from .workers.report import generate_excel_report
 from core.settings import SETTINGS
+from tasks.arq_tasks.workers.report import generate_excel_report
 
 
+async def main():
+    worker = Worker(
+        functions=[generate_excel_report],
+        redis_settings=RedisSettings.from_dsn(SETTINGS.REDIS_URL),
+        max_jobs=10,
+        job_timeout=300,
+    )
+    print("✅ Successfully Worker Started")
+    await worker.async_run()
 
-class WorkerSettings:
-    functions = [generate_excel_report]
 
-    redis_settings = RedisSettings.from_dsn(SETTINGS.REDIS_URL)
-
-    max_jobs = 10
-    job_timeout = 300
+if __name__ == "__main__":
+    asyncio.run(main())
